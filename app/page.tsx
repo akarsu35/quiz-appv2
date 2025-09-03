fe70af0000fef0e8e237603b3454f0349cd083e5
akarsu35 <cumhurakarsu@gmail.com>
2025-09-03 22:11:21 +0300

    responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1103,7 +1103,7 @@ function QuizGame({
                   </CardTitle>
                 </CardHeader>
                 <CardContent>
-                  <div className="grid grid-cols-2 gap-2">
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
                     {currentQuestion.options.map((option, index) => (
                       <Button
                         key={index}
